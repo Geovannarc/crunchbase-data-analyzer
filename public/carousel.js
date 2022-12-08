d2bf2fe9425ad829080a7b07ec32df2d60bd2e0e
Leon Cohen <cohenleonlbc@gmail.com>
2022-12-07 23:10:03 -0300

    carousel fix

--- public/carousel.js
+++ public/carousel.js
@@ -4,17 +4,17 @@ if(window.matchMedia("(min-width:576px)").matches){
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: false,
-  wrap: false
+  wrap: true
 })
 
 let carouselWidth = $('.carousel-inner')[0].scrollWidth;
-let cardWidth = $('.carousel-item').width();
+let cardWidth = $('.carousel-wrapper').width();
 
 let scrollPosition = 0;
 
 
 $('.carousel-control-next').on('click', function(){
-    if(scrollPosition < (carouselWidth - ((cardWidth+100) * 3))){
+    if(scrollPosition < (carouselWidth - ((cardWidth) * 3))){
     scrollPosition = scrollPosition + cardWidth+100;
     $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600);
     }
